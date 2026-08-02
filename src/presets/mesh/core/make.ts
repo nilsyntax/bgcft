32beb6c2b852370f0c489f5dc6b55b8138006611
Nilesh Roy <nileshroy404@gmail.com>
2026-08-02 23:41:06 +0530

    Generate svg data for mesh gradient

--- src/presets/mesh/core/make.ts
+++ src/presets/mesh/core/make.ts
@@ -1,37 +1,33 @@
-import { pickOnce } from "../../../utils/pickOnce";
-import { random } from "../../../utils/random";
+import { random } from "../../../utils/random.ts";
+import { pickItem, pickItems } from "../../../utils/pickItems.ts";
 
-import { paths } from "../config/paths";
-import { bgColor, colorPalette } from "../config/colors";
-import { layouts } from "../config/layout";
-
-// type makeProps = {
-//    shape: string
-//    color: string
-// }
+import { paths } from "./config/paths.ts";
+import { bgColor, colorPalette } from "./config/colors.ts";
+import { layouts } from "./config/layout.ts";
 
 
-function pickColor(){
-   const pickColor = colorPalette[random(0, colorPalette.length)]
+export function make() {
+   const count = random(2, 3)
+   console.log(count)
+   const svgPaths = pickItems(paths, count)
+   const layoutDat = pickItem(layouts[count])
+
+   const theme = pickItem(colorPalette)
+   const colors = pickItems(theme.colors, count)
+   const bgColorId = pickItem(theme.bgColor)
+
+   // return bgColorId
+   return {
+      bgColorId,
+
+      shapeDat: svgPaths.map((path, i) => ({
+         path,
+         color: colors[i],
+         position: layoutDat[i],
+         scale: 4,
+         blur: 30
+      }))
+   }
 }
-// const pickColor = colorPalette[random(0, colorPalette.length)]
 
-console.log(pickColor)
-/*
-A function Return mesh recipie data
-shape (d=path actually)
-position
-color
-scale
-blur
-
-as ts rules function should have type defination for returning value
-
-
-*/ 
-
-// git commit -m "Add new config directory, write configs layers to new seperate files.
-// type.ts: add type defination
-// paths.ts: store paths(d) to render shape for svg
-// layout.ts: define better layout and position structure for shapes to place inside svg accuratly
-// color.ts: define color scheme and palette for shapes and mesh background"
+console.dir(make(), {depth: null})

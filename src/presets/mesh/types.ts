ed7c7a2c1c8ec4499614f2cea3acc86e59973553
Nilesh Roy <nileshroy404@gmail.com>
2026-08-03 18:00:39 +0530

    add better type defination

---
+++ src/presets/mesh/types.ts
@@ -0,0 +1,49 @@
+// defination for svg shape paths
+export interface Path {
+  d: string
+  width: number
+  height: number
+  center: {
+    x: number
+    y: number
+  }
+}
+
+// defination for shape position
+export interface Position {
+  x: number
+  y: number
+}
+
+export type Layout = Position[]
+
+export type LayoutMap = {
+  [count: number] : Layout[]
+}
+
+export interface ShapeProps {
+  path: Path
+  color: string
+  position: Position
+  scale: number
+  blur: number
+}
+
+export type SvgProps = {
+   background: string;
+   shapeDat: ShapeProps[];
+}
+
+// defination for color palette
+export interface Backgrounds {
+  id: string
+  color: string
+}
+
+export interface ColorPalette {
+  id: string
+  colors: string[]
+  bgColor: string[]  // background color id
+}
+
+

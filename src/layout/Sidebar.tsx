0e1ec9f2d077c6c3406cfa54e58132a393611cdd
Nilesh Roy <nileshroy404@gmail.com>
2026-08-07 19:41:44 +0530

    UI updated with rendered slider

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,31 +1,39 @@
 import HidePanel from "@/assets/icons/hidepanel.svg?react";
 import Button from "../components/Button";
+import { ToolView } from "../presets/mesh/controls/ToolView";
+import type { SvgProps } from "../presets/mesh/types";
 
-type SidebarProps = { onGenerate: () => void; }
 
-function Sidebar({onGenerate}: SidebarProps) {
+type SidebarProps = {
+   onGenerate: () => void;
+   mesh: SvgProps
+   onEffectChange: (id: string, value: number) => void
+}
+
+function Sidebar({ onGenerate, mesh, onEffectChange }: SidebarProps) {
 
    const sidebarClass = `flex flex-col gap-4 h-full w-60 min-w-60 rounded-xl bg-gray-50/0 px-0 shadow-[0_8px_30px_rgba(0,0,0,0.0)]`
-   
+
    const sidebarTitleClass = `flex justify-between items-center h-fit w-full my-0 rounded-xl bg-gray-50/60 px-3 py-1 shadow-[0_8px_30px_rgba(0,0,0,0.08)]`
-   
-   const toolSectionClass = `relative flex flex-col justify-between items-center h-full w-full my-0 rounded-xl bg-gray-50/60 px-3 py-1.5 shadow-[0_8px_30px_rgba(0,0,0,0.08)]`
-   
+
+
+
    return (
       <>
          <div id="sidebar" className={`${sidebarClass}`}>
 
             <div className={`${sidebarTitleClass}`}>
-               <p className="text-l">BGCRAFT</p>
+               <p className="text-l">A Name🙏</p>
                <HidePanel className="text-gray-600 w-8 h-8 p-1 cursor-pointer" />
             </div>
+            <div className="toolArea relative flex flex-col items-center h-full w-full my-0 rounded-xl bg-gray-50/60 px-3 py-1.5 shadow-[0_8px_30px_rgba(0,0,0,0.08)]">
 
-            <div className={`${toolSectionClass}`}>
-               <p className="px-3 py-1.5 text-2xl"></p>
+               <ToolView mesh={mesh} onEffectChange={onEffectChange} />
                <Button
-               value="Randomize"
-               onClick={onGenerate}
-               className="w-full bg-violet-500"/>
+                  value="Randomize"
+                  onClick={onGenerate}
+                  className="w-full bg-violet-500"
+               />
             </div>
 
          </div>
